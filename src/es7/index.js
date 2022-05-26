c102dd020a3e4ed000ce0e08fb19c222c988638e
diegoolipa <d.diegolipa@gmail.com>
2022-05-26 09:09:37 -0500

    es7 es8

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+// Includes: Trabaja sobre un arreglo o string, nos
+// permite saber si hay un elemento dentro de este valor.
+// Es uan forma muy útil de validar elementos dentro de.
+
+let number =[1,2,3,4,6];
+
+if(number.includes(6)){
+    console.log("Se encuentra el valor");
+}else{
+    console.log("No se encuentra el valor");
+}
+
+//Elevar a la potencia:
+
+let base =4;
+let exponent =3;
+let resultado = base**exponent;
+
+console.log(resultado);
